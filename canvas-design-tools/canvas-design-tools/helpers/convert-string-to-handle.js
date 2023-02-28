2b18633ac5598684942031ca92ab1f15be124c05
Craig Baldwin <craig@wemakewebsites.com>
2023-02-28 09:21:14 +0000

    REFACTOR: Updated classes API to use helper functions

--- canvas-design-tools/canvas-design-tools/helpers/convert-string-to-handle.js
+++ canvas-design-tools/canvas-design-tools/helpers/convert-string-to-handle.js
@@ -5,10 +5,30 @@
  *
  * @param {String} string - String to convert.
  * @param {Object} config - Design config.
+ * @param {Boolean} isClass - Is a class conversion.
+ * @param {String|Boolean} [parent] - String parent to combine.
  * @returns {String}
  */
-module.exports = (string, config) => {
+module.exports = (string, config, isClass, parent = false) => {
   const handle = string && string.replaceAll('+', 'Plus')
+
+  if (isClass) {
+    const newHandle = handle
+      .match(/[A-Z0-9]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z0-9]+[0-9]*|[A-Z]|[0-9]+/g)
+      .map((part) => part.toLowerCase())
+      .join(config.delimiter)
+
+    if (!parent) {
+      return handle
+    }
+
+    const parentHandle = parent
+      .match(/[A-Z0-9]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z0-9]+[0-9]*|[A-Z]|[0-9]+/g)
+      .map((part) => part.toLowerCase())
+      .join(config.delimiter)
+
+    return `${parentHandle}${config.delimiter}${newHandle}`
+  }
 
   return handle
     .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z0-9]+[0-9]*|[A-Z]|[0-9]+/g)
--- canvas-design-tools/canvas-design-tools/helpers/is-excluded-not-included.js
+++ canvas-design-tools/canvas-design-tools/helpers/is-excluded-not-included.js
@@ -5,12 +5,24 @@
  * - Can target specific values, e.g. 'text.text-body-m'.
  * - Used to determine if current key shouldn't be rendered.
  * - Returns true if key is excluded or is not included.
+ * - If class is a default class then it cannot be excluded.
  * @param {String} key - Type of class, e.g. 'text'. If testing value then pass
  * in format `[key].[className]`, e.g. 'text.text-body-m'.
  * @param {Object} stylesheet - Stylesheet object.
+ * @param {Object} config - Design config.
  * @returns {Boolean}
  */
-module.exports = (key, stylesheet) => {
+module.exports = (key, stylesheet, config) => {
+  if (config) {
+    const defaultClass = Object.values(config.defaults).some((value) => {
+      return `typography.${value}` === key
+    })
+
+    if (defaultClass || key === 'typography.html, body') {
+      return false
+    }
+  }
+
   if (stylesheet.include.length) {
     const included = stylesheet.include.some((item) => {
       const test = item.includes('.') && !key.includes('.')
@@ -33,6 +45,26 @@ module.exports = (key, stylesheet) => {
     })
 
     return excluded
+  }
+
+  /**
+   * If critical stylesheet and matches default class name or html, body then
+   * include in stylesheet render.
+   * - Only check when in format `[key].[className]`.
+   */
+  if (key.includes('.') && stylesheet.handle === 'classes-critical') {
+    const included = Object.entries(config.defaults).some(([defaultName, defaultClassName]) => {
+      if (defaultName === 'button') {
+        return false
+      }
+
+      return (
+        `${config.defaultsType}.${config.special.htmlBody}` === key ||
+        `${config.defaultsType}.${defaultClassName}` === key
+      )
+    })
+
+    return !included
   }
 
   return false
