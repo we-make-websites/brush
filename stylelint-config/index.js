f0aecf2513b6bd05f71eef0c6f5c0e575cf7a294
Craig Baldwin <craig@wemakewebsites.com>
2023-04-26 10:41:17 +0100

    REFACTOR: Added stylelint-stylistic, reset rules to use default configs

--- stylelint-config/index.js
+++ stylelint-config/index.js
@@ -4,7 +4,7 @@
  * We Make Websites stylelint configuration.
  *
  */
-const merge = require('merge')
+// const merge = require('merge')
 
 module.exports = {
   overrides: [
@@ -16,31 +16,36 @@ module.exports = {
   plugins: [
     'stylelint-scss',
     'stylelint-order',
+    'stylelint-stylistic',
     './plugins/content-no-strings',
   ],
-  rules: merge(
-    require('./rules/at-rule'),
-    require('./rules/block'),
-    require('./rules/color'),
-    require('./rules/comment'),
-    require('./rules/declaration'),
-    require('./rules/font'),
-    require('./rules/function'),
-    require('./rules/general'),
-    require('./rules/length'),
-    require('./rules/media'),
-    require('./rules/number'),
-    require('./rules/order'),
-    require('./rules/property'),
-    require('./rules/rule'),
-    require('./rules/scss'),
-    require('./rules/selector'),
-    require('./rules/string'),
-    require('./rules/time'),
-    require('./rules/unit'),
-    require('./rules/value'),
-    {
-      'shopify/content-no-strings': null,
-    },
-  ),
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-stylistic/config',
+  ],
+  // rules: merge(
+  //   require('./rules/at-rule'),
+  //   require('./rules/block'),
+  //   require('./rules/color'),
+  //   require('./rules/comment'),
+  //   require('./rules/declaration'),
+  //   require('./rules/font'),
+  //   require('./rules/function'),
+  //   require('./rules/general'),
+  //   require('./rules/length'),
+  //   require('./rules/media'),
+  //   require('./rules/number'),
+  //   require('./rules/order'),
+  //   require('./rules/property'),
+  //   require('./rules/rule'),
+  //   require('./rules/scss'),
+  //   require('./rules/selector'),
+  //   require('./rules/string'),
+  //   require('./rules/time'),
+  //   require('./rules/unit'),
+  //   require('./rules/value'),
+  //   {
+  //     'shopify/content-no-strings': null,
+  //   },
+  // ),
 }
