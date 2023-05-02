9682ca5bbdab3b1610d8bb47ac0627cbfa58d001
Craig Baldwin <craig@wemakewebsites.com>
2023-05-02 15:51:24 +0100

    RELEASE: 4.0.0 (stylelint-config)
    Future support for stylelint 16, updated rules, updated dependencies

--- stylelint-config/rules/at-rule.js
+++ stylelint-config/rules/at-rule.js
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-
 const message = (rule) => {
   switch (rule) {
     case 'debug':
@@ -12,17 +10,20 @@ const message = (rule) => {
 }
 
 module.exports = {
-  // Specify a list of disallowed at-rules.
+  // Specify a list of allowed at-rules
+  'at-rule-allowed-list': null,
+  // Specify a list of disallowed at-rules
   'at-rule-disallowed-list': [['debug', 'error', 'warn'], { message }],
-  // Require or disallow an empty line before @rules.
-  'at-rule-empty-line-before': null,
-  // Specify lowercase or uppercase for at-rules names.
-  'at-rule-name-case': 'lower',
-  // Require a newline after at-rule names.
-  'at-rule-name-newline-after': 'always-multi-line',
-  // Require a single space after at-rule names.
-  'at-rule-name-space-after': 'always-single-line',
-  // Disallow unknown at-rules.
+  // Require or disallow an empty line before @rules
+  'at-rule-empty-line-before': [
+    'always',
+    {
+      except: ['first-nested'],
+      ignore: ['after-comment', 'blockless-after-same-name-blockless'],
+      ignoreAtRules: ['else', 'include', 'extend'],
+    },
+  ],
+  // Disallow unknown at-rules
   'at-rule-no-unknown': [
     true, {
       ignoreAtRules: [
@@ -45,12 +46,6 @@ module.exports = {
       ],
     },
   ],
-  // Disallow vendor prefixes for @rules.
+  // Disallow vendor prefixes for @rules
   'at-rule-no-vendor-prefix': true,
-  // Require a newline after the semicolon of at-rules.
-  'at-rule-semicolon-newline-after': 'always',
-  // Specify a list of allowed at-rules.
-  'at-rule-allowed-list': null,
-  // Require a single space or disallow whitespace before the semicolons of at-rules.
-  'at-rule-semicolon-space-before': 'never',
 }
